5f9faf7ee81beea987261d8eb59df98f5541bdf3
Samuel Chen <sche842@gmail.com>
2024-08-05 22:30:59 +1000

    two sums

---
+++ 001_two_sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsSorted = nums.copy()
+        numsSorted.sort()
+        result = []
+        print(numsSorted)
+        p1 = 0
+        p2 = len(numsSorted) - 1
+        while p1 < p2:
+            if (numsSorted[p1] + numsSorted[p2]) > target:
+                p2 -= 1
+            elif (numsSorted[p1] + numsSorted[p2]) < target:
+                p1 += 1
+            else:
+                x = numsSorted[p1]
+                y = numsSorted[p2]
+                for i in range(0, len(nums)):
+                    if nums[i] == x:
+                        result.append(i)
+                    elif nums[i] == y:
+                        result.append(i)
+                break
+
+        return result
