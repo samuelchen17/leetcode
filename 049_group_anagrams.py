cc428f54e78f26dc4840130e91c7da2430bc05c5
Samuel Chen <sche842@gmail.com>
2024-08-10 23:05:14 +1000

    049

---
+++ 049_group_anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        result = []
+
+        for s in strs:
+            sorted_s = tuple(sorted(s))  # convert to tuple
+            anagram_map[sorted_s].append(s)
+
+        for value in anagram_map.values():
+            result.append(value)
+
+        return result
