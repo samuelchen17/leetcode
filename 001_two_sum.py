17ea9ff62d13dca8c0c1473be5c476d93502a581
Samuel Chen <sche842@gmail.com>
2024-08-15 23:30:48 +1000

    fix

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         # hashmap
         # key = value
         # value = index
