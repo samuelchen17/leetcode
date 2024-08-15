1421cf2d82a3e38233f23730a67acdb1bc5fdfa7
Samuel Chen <sche842@gmail.com>
2024-08-16 00:51:44 +1000

    valid paren

--- 020_valid_parentheses.py
+++ 020_valid_parentheses.py
@@ -6,14 +6,22 @@ class Solution:
         bracket_map = {")": "(", "]": "[", "}": "{"}
         stack = []
 
+        # basically whenever we come across a closing parentheses, the top of stack must be the same
         for c in s:
+            # check if this character is a closing parentheses
             if c in bracket_map:
+                # check if parentheses is empty
+                # and top of the stack is the same as the closing parentheses
+                # stack[-1] is the last value added
                 if stack and stack[-1] == bracket_map[c]:
                     stack.pop()
+                # if dont match or empty, that means it parentheses dont match
                 else:
                     return False
 
+            # as long as it is open parentheses, can keep adding to stack
             else:
                 stack.append(c)
 
+        # only true if stack is empty, this means every opening parentheses has been matched
         return True if not stack else False
