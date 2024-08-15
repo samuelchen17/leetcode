44c86c19404294ade29f5a9d6d4bc7e1702ba9d7
Samuel Chen <sche842@gmail.com>
2024-08-16 01:03:27 +1000

    merge two sorted list notes

--- 021_merge_two_sorted_lists.py
+++ 021_merge_two_sorted_lists.py
@@ -9,13 +9,18 @@ class Solution:
         dummy = ListNode()
         tail = dummy
 
+        # while list1 and list2 are not null, so they can be compared
         while list1 and list2:
+            # if value of list1 is less than list2, insert it into tail
             if list1.val < list2.val:
                 tail.next = list1
+                # update list1 pointer
                 list1 = list1.next
             else:
+                # insert list2 value as tail and update pointer
                 tail.next = list2
                 list2 = list2.next
+            # tail node updated regardless, hence outside
             tail = tail.next
 
         if list1:
