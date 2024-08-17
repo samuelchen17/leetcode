9e7eb41626e2f54e76a6db5f8f71528fd5c3f98e
Samuel Chen <sche842@gmail.com>
2024-08-17 23:20:16 +1000

    two sum

---
+++ neetcode/two_int_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        return indices of two numbers that add to target
+        create hashmap, the key will be number, value will be index
+        loop through array
+        check for difference
+        check if difference exists in the hashmap
+        if yes, return index and value from hashmap
+        if not, add to hashmap
+        """
+        int_map = {}
+        result = []
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference in int_map:
+                return [int_map[difference], i]
+            else:
+                int_map[num] = i
