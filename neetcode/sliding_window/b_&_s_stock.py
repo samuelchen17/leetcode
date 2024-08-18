dd7a3e1de54796a237dfc21f3b0a047210515132
Samuel Chen <sche842@gmail.com>
2024-08-18 23:07:13 +1000

    best time to buy and sell stock

---
+++ neetcode/sliding_window/b_&_s_stock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        """
+        essentially find the greatest difference between two numbers in the list
+        big number has to go first
+
+        only need to check which days make the most profit, hence move p1 to p2
+
+        make two pointers
+        set pointer 1 to be 0 and p2 to be 1
+        check if next value is bigger
+        if bigger, check for profit
+        compare with max profit
+        else move p1 to p2
+        increment s
+        """
+
+        b, s = 0, 1
+        max_profit = 0
+
+        while s < len(prices):
+            if prices[b] < prices[s]:
+                profit = prices[s] - prices[b]
+                max_profit = max(max_profit, profit)
+            else:
+                b = s
+            s += 1
+
+        return max_profit
