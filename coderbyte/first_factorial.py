e83c1d20cc1efbf875f8b9dc1ea31f157dafd468
Samuel Chen <sche842@gmail.com>
2024-08-19 16:24:49 +1000

    first fact

--- coderbyte/first_factorial.py
+++ coderbyte/first_factorial.py
@@ -4,3 +4,15 @@ def fact(num):
         return 1
     else:
         return num * fact(num - 1)
+
+
+def FirstFactorial(num):
+    """
+    recursively solve this
+    """
+
+    # base case
+    if num == 0:
+        return 1
+
+    return num * FirstFactorial(num - 1)
--- coderbyte/first_reverse.py
+++ coderbyte/first_reverse.py
@@ -17,3 +17,30 @@ def FirstReverse(strParam):
 
 # keep this function call here
 print(FirstReverse(input()))
+
+
+def FirstReverseTwoPointer(strParam):
+    """
+    depends if to do in place or create new space
+    in place:
+    create p1 and p2
+    if p1 == p2
+      return the str
+    elif
+    """
+    char_list = list(strParam)
+
+    p1, p2 = 0, len(strParam) - 1
+    while p1 < p2:
+        char_list[p1], char_list[p2] = char_list[p2], char_list[p1]
+        p1 += 1
+        p2 -= 1
+
+    return "".join(char_list)
+
+    # code goes here
+    return strParam
+
+
+# keep this function call here
+print(FirstReverseTwoPointer(input()))
